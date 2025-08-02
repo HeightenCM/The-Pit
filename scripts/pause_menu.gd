7425cf1c28cdd6047284844c20f9f69029b91b39
GabrielLeanca <gabriel.leanca@gmail.com>
2025-08-02 18:55:58 +0300

    put empty scenes for inventory and ammo slots in pause menu

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -4,6 +4,9 @@ extends Control
 var revolver_slots := []
 var shotgun_slots := []
 var rifle_slots := []
+@onready var ammo_hbox = $ScrollContainer/Ammo_HBox
+@onready var inventory_container = $Inventory_GridContainer
+@export var slot_scene: PackedScene
 
 func _on_resume() -> void:
 	ui.visible = true
@@ -20,6 +23,7 @@ func _on_button_pressed() -> void:
 
 
 func _on_ready() -> void:
+	slot_scene = load("res://scenes/slot_scene.tscn")
 	visible = false
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	revolver_slots.resize(6)
@@ -28,3 +32,19 @@ func _on_ready() -> void:
 	shotgun_slots.fill(null)
 	rifle_slots.resize(6)
 	rifle_slots.fill(null)
+	refresh_slots()
+	populate_inventory()
+	
+func refresh_slots() -> void:
+	for child in ammo_hbox.get_children():
+		child.queue_free()
+	for i in range(revolver_slots.size()):
+		var slot = slot_scene.instantiate()
+		ammo_hbox.add_child(slot)
+		print("Adding slot", i)
+
+func populate_inventory() -> void:
+	for i in range(40):
+		var slot = slot_scene.instantiate()
+		inventory_container.add_child(slot)
+		print("added inventory slot", i)
