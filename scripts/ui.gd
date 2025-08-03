0142310feafc8afc0b9dc78a4ce3d7c8c7c40d44
GabrielLeanca <gabriel.leanca@gmail.com>
2025-08-03 16:03:26 +0300

    make waves not start if ammo not equipped

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -5,6 +5,7 @@ var currentHealth : int = maxHealth
 @onready var pause_menu = get_parent().get_node("PauseLayer").get_node("PauseMenu")
 var start_wave_button
 var enemies_left_label
+var player
 
 
 func set_hp(hp):
@@ -20,14 +21,27 @@ func _on_button_pressed() -> void:
 
 
 func _on_start_wave_button_pressed() -> void:
+	if is_empty():
+		return
 	get_tree().get_current_scene().start_wave()
 	start_wave_button.visible = false
 	enemies_left_label.text = "Enemies left: " + str(get_tree().get_current_scene().enemy_counter)
 	enemies_left_label.visible = true
 	
-
+func is_empty() -> bool:
+	for slot in player.revolver_slots:
+		if slot != "":
+			return false
+	for slot in player.shotgun_slots:
+		if slot != "":
+			return false
+	for slot in player.rifle_slots:
+		if slot != "":
+			return false
+	return true
 
 func _on_ready() -> void:
+	player = get_tree().get_current_scene().get_node("Pete")
 	enemies_left_label = get_node("EnemiesLeftLabel")
 	enemies_left_label.visible = false
 	start_wave_button = get_node("HBoxContainer3/StartWaveButton")
